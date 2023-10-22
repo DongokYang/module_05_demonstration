ac4016d49f5474dba650241e3e17ab2dce40d3e0
DongokYang <dyang2@academic.rrc.ca>
2023-10-22 18:20:48 -0500

    starting file set up

---
+++ module_5_functions/module_5_functions/functions.py
@@ -0,0 +1,7 @@
+"""
+Description: Module 05 demonstration: Functions
+Author: {student name}
+Date: {current date}
+Usage: To run individual functions, place a function 
+call within the main guard.
+"""
